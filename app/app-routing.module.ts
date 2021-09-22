470a1983bbaeb51eeadf166c361139bc3cb6180e
Antoine Ceol <antoine.ceol@gmail.com>
2021-09-22 09:31:03 +0200

    :rocket: first commit for labs

---
+++ app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { BasketComponent } from './basket/basket.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'basket', component: BasketComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ app/basket/basket.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Product } from '../model/product';
+import { Customer } from '../model/customer';
+
+@Component({
+  selector: 'app-basket',
+  templateUrl: './basket.component.html',
+  styleUrls: ['./basket.component.scss']
+})
+export class BasketComponent {
+  basket: Product[] = [];
+  customer: Customer;
+
+  constructor(private router: Router) {
+    this.basket = (window as any).basket;
+    this.customer = new Customer();
+  }
+
+  checkout() {
+    (window as any).basket = [];
+    this.router.navigate(['']);
+  }
+}

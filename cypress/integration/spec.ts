913052d88dee424dcc559046f1465c102026e767
Antoine Ceol <antoine.ceol@gmail.com>
2021-09-23 12:16:36 +0200

    Lab-2 add cypress test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -4,4 +4,14 @@ describe('My First Test', () => {
     cy.contains('Bienvenue sur Zenika Ecommerce');
     cy.contains('Voir mon panier');
   });
+
+  describe('Add to basket', () => {
+    it('Add to the basket and update the total', () => {
+      cy.visit('/');
+
+      cy.get('button').contains('Ajoutez au panier').click();
+
+      cy.contains("Votre panier s'élève à €20.00");
+    });
+  });
 });
